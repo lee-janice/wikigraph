2b54a9f21d4eaf88a8deb526ba93635c49316391
lee-janice <janicel375@gmail.com>
2022-11-28 20:34:26 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WikiGraph from './wikigraph';
+
+const NEO4J_DB = String(process.env.REACT_APP_NEO4J_DB);
+const NEO4J_URI = String(process.env.REACT_APP_NEO4J_URI);
+const NEO4J_USER = String(process.env.REACT_APP_NEO4J_USER);
+const NEO4J_PASSWORD = String(process.env.REACT_APP_NEO4J_PASSWORD);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+		<div className="App">
+			<WikiGraph
+				width={700}
+				height={600}
+				containerId={"viz"}
+				serverDatabase={NEO4J_DB}
+				serverURI={NEO4J_URI}
+				serverUser={NEO4J_USER}
+				serverPassword={NEO4J_PASSWORD}
+			/>
+		</div>
+    )
 }
 
 export default App;
---
+++ src/wikigraph.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useRef } from "react";
+import NeoVis from "neovis.js/dist/neovis.js";
+import './vis.css';
+
+interface Props {
+    width: number, 
+    height: number,
+    containerId: string,
+    serverDatabase: string,
+    serverURI: string,
+    serverUser: string,
+    serverPassword: string,
+};
+
+const WikiGraph: React.FC<Props> = ({
+    width, 
+    height, 
+    containerId, 
+    serverDatabase, 
+    serverURI,
+    serverUser,
+    serverPassword,
+}) => {
+    useEffect(() => {
+        var config = {
+            containerId: containerId,
+            // neo4j database connection settings 
+            serverDatabase: serverDatabase, // specify which database to read from 
+            neo4j: {
+                serverUrl: serverURI,
+                serverUser: serverUser,
+                serverPassword: serverPassword
+            },
+            // override the default vis.js settings 
+            visConfig: {
+                nodes: {
+                    shape: "circle",
+                    color: {
+                        background: "lightgray",
+                        border: "gray",
+                        highlight: {
+                            background: "lightgray",
+                            border: "gray",
+                        },
+                    },
+                },
+                edges: { arrows: { to: { enabled: true, }, }, },
+                physics: {
+                    // uses the Barnes-Hut algorithm to compute node positions
+                    barnesHut: { avoidOverlap: 1 }, // value between 0 and 1, 1 is maximum overlap avoidance 
+                },
+                layout: { randomSeed: 47 },
+                interaction: { multiselect: true }, // allows for multi-select using a long press or cmd-click }
+            },
+            // node and edge settings 
+            labels: {
+                Page: {
+                    label: "title",
+                    size: "clicksInto",
+                },
+            },
+            relationships: {
+                LINKS_TO: {
+                    value: "quantity",
+                },
+            },
+            initialCypher: "MATCH (p1:Page)-[l:LINKS_TO]->(p2:Page) \
+                            WHERE p1.title = 'Universe' \
+                            RETURN p1, l, p2 \
+                            ORDER BY l.quantity DESC \
+                            LIMIT 10"
+        };
+        const viz = new NeoVis(config);
+        viz.render();
+    }, []);
+    return (<div id={containerId} style={{ width: `${width}px`, height: `${height}px`, }}></div>);
+};
+
+export default WikiGraph;
