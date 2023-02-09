57ccdd7fce313990a174d236e97c583dff9edf8b
lee-janice <janicel375@gmail.com>
2023-02-09 18:45:39 -0500

    re-enable `React.StrictMode` https://github.com/lee-janice/wikigraph/issues/6

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./App";
+import React from "react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  // React.StrictMode double-invokes renders, so this causes a glitch in the graph visualization 
-  // https://stackoverflow.com/questions/48846289/why-is-my-react-component-is-rendering-twice
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+    // React.StrictMode double-invokes renders, so this causes a glitch in the graph visualization
+    // https://stackoverflow.com/questions/48846289/why-is-my-react-component-is-rendering-twice
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
