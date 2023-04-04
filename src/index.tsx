20dccd7352c5c42cc518f360855bf30308684275
lee-janice <janicel375@gmail.com>
2023-04-04 11:15:49 -0400

    update lightmode color to be not as sickly

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,6 @@ import React from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-    // React.StrictMode double-invokes renders, so this causes a glitch in the graph visualization
-    // https://stackoverflow.com/questions/48846289/why-is-my-react-component-is-rendering-twice
     <React.StrictMode>
         <App />
     </React.StrictMode>
